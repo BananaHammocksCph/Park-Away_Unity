f0151ccfd9d10c362e8c25c49476c182b9c86200
FarkIst <FarkIst@github.com>
2019-11-26 16:07:47 +0100

    image test

---
+++ Assets/Farki/Scripts/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public Image cameraDisplayImage, confirmCaptureImage;
+    public GameObject confirmCapPanel;
+    private byte[] _screenshot;
+
+
+    #region Properties
+
+    public byte[] Screenshot
+    {
+        get { return _screenshot; }
+
+        set { _screenshot = value; }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraDisplayImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnCamera()
+    {
+        WebCamTexture webCamTexture = new WebCamTexture();
+
+        cameraDisplayImage.gameObject.SetActive(true);
+        cameraDisplayImage.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    public void OnCaptureButton()
+    {
+        //disable elements for screencapture
+
+        foreach(Transform t in cameraDisplayImage.transform)
+        {
+            t.gameObject.SetActive(false);
+        }
+
+        // Capture
+        CaptureScreenshot();
+
+        confirmCapPanel.SetActive(true);
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D texture = new Texture2D(width, height);
+        texture.LoadImage(Screenshot);
+    }
+
+    private void CaptureScreenshot()
+    {
+        byte[] imageArray;
+
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+
+        imageArray = tex.EncodeToPNG();
+
+
+        Destroy(tex);
+        Screenshot = imageArray;
+    }
+
+
+
+
+}
--- Assets/Farki/Scripts/PublishHelper.cs
+++ Assets/Farki/Scripts/PublishHelper.cs
@@ -1,4 +1,5 @@
 ﻿using CymaticLabs.Unity3D.Amqp;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -10,6 +11,7 @@ public class PublishHelper : MonoBehaviour
     [SerializeField] private string _exchangeName, _routingKey;
     public AmqpExchangeTypes ExchangeType = AmqpExchangeTypes.Direct;
 
+    [SerializeField] private CameraController _cameraController;
     public RawImage userImage;
 
     private User _user;
@@ -20,46 +22,53 @@ public class PublishHelper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var subscription = new AmqpExchangeSubscription(_exchangeName, ExchangeType, _routingKey, HandleExchangeMessageReceived);
+        AmqpClient.Subscribe(subscription);
+
         _user = new User();
+        if (_cameraController == null)
+            _cameraController = FindObjectOfType<CameraController>();
+    }
+
+    private void HandleExchangeMessageReceived(AmqpExchangeReceivedMessage received)
+    {
+        var receivedJson = System.Text.Encoding.UTF8.GetString(received.Message.Body);
+        DeserializeUser(receivedJson);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("p"))
-            SerializeUser();
+            Publish();
 
         if (Input.GetKeyDown("r"))
             DeserializeUser(_jsonString);
     }
 
-    private byte[] GetImage()
-    {
-        byte[] imageArray;
+    //private byte[] GetImage()
+    //{
+    //    byte[] imageArray;
 
-        int width = Screen.width;
-        int height = Screen.height;
-        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
-        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        tex.Apply();
+    //    int width = Screen.width;
+    //    int height = Screen.height;
+    //    Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+    //    tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+    //    tex.Apply();
 
-        imageArray = tex.EncodeToPNG();
+    //    imageArray = tex.EncodeToPNG();
 
 
-        Destroy(tex);
-        return imageArray;
-    }
+    //    Destroy(tex);
+    //    return imageArray;
+    //}
 
     public void DeserializeUser(string json)
     {
         int width = Screen.width;
         int height = Screen.height;
 
-        using (StreamReader reader = new StreamReader(Application.dataPath + "/TestJson.json"))
-        {
-            string j = reader.ReadToEnd();
-            _user = JsonUtility.FromJson<User>(j);
-        }
+        _user = JsonUtility.FromJson<User>(json);
 
         Texture2D texture = new Texture2D(width, height);
         texture.LoadImage(_user.image);
@@ -69,7 +78,7 @@ public class PublishHelper : MonoBehaviour
 
     public void SerializeUser()
     {
-        User user = new User
+        _user = new User
         {
             id = "Test",
             coordinates = new Coordinates
@@ -77,26 +86,24 @@ public class PublishHelper : MonoBehaviour
                 latitude = 12.090,
                 longitude = 55.665
             },
-            image = GetImage()
+            image = _cameraController.Screenshot
         };
 
-        File.WriteAllBytes(Application.dataPath + "/TestImage", GetImage());
+        //File.WriteAllBytes(Application.dataPath + "/TestImage", GetImage());
 
         using (StreamWriter stream = new StreamWriter(Application.dataPath + "/TestJson.json"))
         {
-            string json = JsonUtility.ToJson(user, true);
+            string json = JsonUtility.ToJson(_user, true);
             _jsonString = json;
             stream.Write(json);
         }
-
-        Debug.Log("PATH : " + Application.dataPath);
     }
 
 
     public void Publish()
     {
-        string str = "\"id\":\"cube1\", \"posx\":2, \"posY\":0.5f, \"posZ\"-0.2f";
-        var message = str;
-        AmqpClient.Publish(_exchangeName, _routingKey, message);
+        var msg = JsonUtility.ToJson(_user);
+
+        AmqpClient.Publish(_exchangeName, _routingKey, msg);
     }
 }
